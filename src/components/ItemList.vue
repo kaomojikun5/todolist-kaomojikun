6557682a2f5b85f34036e67d3f85641172487d9e
kaomojikun <kaomojikun5@gmail.com>
2025-06-11 14:57:56 +0900

    kougaku

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -31,6 +31,7 @@ const addItem = () => {
       <li v-for="item in items" :key="item.name" :class="{ over500: item.price >= 500 }">
         <div>名前: {{ item.name }}</div>
         <div>{{ item.price }} 円</div>
+        <div v-if="item.price >= 10000">高額商品</div>
       </li>
     </ul>
     <div>
